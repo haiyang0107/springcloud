29409b3ecbbf35707a519f2ee354161d4e25d389
huhaiyang <1208027930@qq.com>
2019-01-10 19:12:58 +0800

    提交代码

--- modules/baseapi/src/main/java/com/springcloud/api/base/BaseDataApi.java
+++ modules/baseapi/src/main/java/com/springcloud/api/base/BaseDataApi.java
@@ -1,5 +1,6 @@
 package com.springcloud.api.base;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,4 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/api/base/baseInfo")
 public interface BaseDataApi {
 
+    @GetMapping(value= "testData")
+    public String testData(String ss);
 }
